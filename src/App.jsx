661986f2a56468c878d159d1e7b481c8c1b2bd83
Furkan Sarı <furkansa@hotmail.com.tr>
2025-07-27 13:56:35 +0300

    feat: add `Footer.jsx` & react-icons & add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Attribution from "./components/Attribution";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <main>
+    <div className="app-wrapper">
+      <main className="main-content">
         <Card />
       </main>
       <Attribution />
-    </>
+      <Footer />
+    </div>
   );
 }
 
